7a5d010e8eb39a58cb52160a0be6aa622ee61b90
Ohata0524 <t3977039@gmail.com>
2026-03-15 19:01:37 +0900

    CIエラーによるPrismaメジャーアップデートによる修正3

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,8 @@
 export default {
   datasource: {
     // CI環境などで環境変数が空の場合、バリデーションエラーを防ぐためにダミーURLを設定します
-    url: process.env.POSTGRES_URL || 'postgresql://postgres:postgres@localhost:5432/postgres',
-  },
+    url:
+      process.env.POSTGRES_URL ||
+      'postgresql://postgres:postgres@localhost:5432/postgres'
+  }
 }
