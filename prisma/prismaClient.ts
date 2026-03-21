55a7f3fd3726917b11d09a9149a86c513674b618
Ohata0524 <t3977039@gmail.com>
2026-03-21 12:28:40 +0900

    fix: PrismaClient初期化

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasourceUrl: process.env.POSTGRES_URL,
+  })
+}
+
+declare global {
+  var globalPrisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.globalPrisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.globalPrisma = prisma
