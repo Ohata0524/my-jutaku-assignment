ffc147db2d7286e6f430ce0bdf3c5bd8637e51a4
Ohata0524 <t3977039@gmail.com>
2026-03-21 13:40:11 +0900

    fix: CIエラーによるYAMLファイルの修正

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,13 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // グローバル変数の衝突を避けるための定義
-  var globalPrisma: undefined | PrismaClient
+	var globalPrisma: undefined | PrismaClient;
 }
 
-// 他のファイルが import { prisma } で読み込めるよう、名前付きエクスポートにします
-export const prisma = globalThis.globalPrisma ?? new PrismaClient()
+export const prisma = globalThis.globalPrisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.globalPrisma = prisma
+if (process.env.NODE_ENV !== "production") {
+	globalThis.globalPrisma = prisma;
 }
